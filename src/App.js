68f992261d64431c85f59997306b4908d26087b3
Ivan Roir <ivanroirbruselas75@gmail.com>
2019-03-04 01:00:19 +0800

    commit

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import './App.css';
-import { BrowserRouter as Router, Route } from "react-router-dom";
+import { HashRouter as Router, Route, Switch } from "react-router-dom";
 import Login from "./Login";
 import Home from "./Home";
 
@@ -8,10 +8,12 @@ class App extends Component {
   render() {
     return (
       <Router>
-        <React.Fragment>
-          <Route exact path="/" component={Login} />
-          <Route exact path="/Home" component={Home} />
-        </React.Fragment>
+        <Switch>
+          <React.Fragment>
+            <Route exact path="/" component={Login} />
+            <Route exact path="/Home" component={Home} />
+          </React.Fragment>
+        </Switch>
       </Router>
     );
   }
